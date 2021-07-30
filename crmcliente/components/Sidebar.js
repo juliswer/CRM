8f93c05d01a68ef17a225310fcfde4a19ac401d7
Julián Swerdlin <julianswer36@gmail.com>
2021-07-30 20:57:31 -0300

    updating routes

--- crmcliente/components/Sidebar.js
+++ crmcliente/components/Sidebar.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import {useRouter} from 'next/router';
 
 const Sidebar = () => {
+
+    // routing de next component
+    const router = useRouter()
+    console.log(router.pathname);
+
     return (
         <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
             <div>
@@ -9,17 +15,25 @@ const Sidebar = () => {
             </div>
 
             <nav className="mt-5 list-none">
-                <li>
+                <li className={router.pathname === '/' ? "bg-blue-800 p-2" : "p-2"}>
                     <Link href="/">
                         <a className="text-white mb-2 block">
                             Clientes
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={router.pathname === '/pedidos' ? "bg-blue-800 p-2" : "p-2"}>
                     <Link href="/pedidos">
                         <a className="text-white mb-2 block">
-                            Clientes
+                            Pedidos
+                        </a>
+                    </Link>
+                </li>
+
+                <li className={router.pathname === '/productos' ? "bg-blue-800 p-2" : "p-2"}>
+                    <Link href="/productos">
+                        <a className="text-white mb-2 block">
+                            Productos
                         </a>
                     </Link>
                 </li>
