18a476fa3947dde22cdbe4e828525693180fe89b
Julián Swerdlin <julianswer36@gmail.com>
2021-07-30 22:02:49 -0300

    uploading new files

--- crmcliente/pages/nuevacuenta.js
+++ crmcliente/pages/nuevacuenta.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik'
+import * as Yup from 'yup';
 
 const NuevaCuenta = () => {
+
+    // Validacion del formulario
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            apellido: '',
+            email: '',
+            password: ''
+        },
+        onSubmit: valores => {
+            console.log('enviando')
+            console.log(valores);
+        }
+    });
+
     return (
         <>
             <Layout>
@@ -11,6 +28,7 @@ const NuevaCuenta = () => {
                   <div className="w-full max-w-sm">
                     <form
                         className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                        onSubmit={formik.handleSubmit}
                     >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
@@ -22,6 +40,8 @@ const NuevaCuenta = () => {
                                 id="nombre"
                                 type="text"
                                 placeholder="Nombre Usuario"
+                                value={formik.values.nombre}
+                                onChange={formik.handleChange}
                             />
                         </div>
                         <div className="mb-4">
@@ -34,6 +54,8 @@ const NuevaCuenta = () => {
                                 id="apellido"
                                 type="text"
                                 placeholder="Apellido Usuario"
+                                value={formik.values.apellido}
+                                onChange={formik.handleChange}
                             />
                         </div>
                         <div className="mb-4">
@@ -46,6 +68,8 @@ const NuevaCuenta = () => {
                                 id="email"
                                 type="email"
                                 placeholder="Email Usuario"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
                             />
                         </div>
                         <div className="mb-4">
@@ -58,6 +82,8 @@ const NuevaCuenta = () => {
                                 id="password"
                                 type="password"
                                 placeholder="Password Usuario"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
                             />
                         </div>
                         
